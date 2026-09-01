d9eb5a323a060d6f73dfe24ca23b1a584b3727ce
Rithwik <98485476+Ritikreal@users.noreply.github.com>
2026-09-01 19:41:04 +0530

    Add files via upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import LoginRegisterModule from './components/LoginRegisterModule';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <LoginRegisterModule />
+    </div>
+  );
+}
+
+export default App;
